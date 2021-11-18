2f351b5771fb6a6cb10454e39f468caca57ed120
dcerriv <dcerrillovacas@gmail.com>
2021-11-18 16:40:19 +0100

    Pruebas_hist_2

--- histogram/histograma.py
+++ histogram/histograma.py
@@ -4,10 +4,10 @@ from matplotlib import pyplot as plt
 img = cv2.imread('nadal.jpg', 0)
 histg = cv2.calcHist([img], [0], None, [256], [0,256])
 
-ret,th1 = cv2.threshold(img, 150, 255, cv2.THRESH_BINARY)
+th2 = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 7, 5)
 
-plt.imshow(th1, 'gray')
+plt.imshow(th2, 'gray')
 plt.show()
 
-cv2.imwrite('nadal_150.jpg', th1)
+cv2.imwrite('nadal_2_15.jpg', th2)
 
