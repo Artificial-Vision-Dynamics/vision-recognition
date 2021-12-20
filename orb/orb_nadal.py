731e716641ac846ba3439b54d823d111b4cd0a38
dcerrillov <dcerrillovacas@gmail.com>
2021-12-20 20:19:28 +0100

    Pruebas con esquiadora

--- orb/orb_nadal.py
+++ orb/orb_nadal.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2 as cv
 from matplotlib import pyplot as plt
 
-img = cv.imread('orb/nadal_mask.jpg',0)
+img = cv.imread('orb/esqui copy.jpg',0)
 
 orb = cv.ORB_create()
 kp = orb.detect(img, None)
@@ -11,7 +11,7 @@ kp, des = orb.compute(img, kp)
 img2 = cv.drawKeypoints(img, kp, None, color=(0,250,0), flags=0)
 plt.imshow(img2)
 plt.show()
-cv.imwrite('orb/orb_keypoints.jpg',img2)
+cv.imwrite('orb/orb_esqui.jpg',img2)
 
 
 
@@ -20,4 +20,4 @@ kp2 = sift.detect(img,None)
 img3=cv.drawKeypoints(img,kp,None)
 plt.imshow(img3)
 plt.show()
-cv.imwrite('orb/sift_keypoints.jpg',img3)
+cv.imwrite('orb/sift_esqui.jpg',img3)
